32c51523d1168448958e08a05494b38107ca3780
jazdosanjh <jas@dosanjh.co.uk>
2019-04-25 19:30:43 +0100

    Made a few colour alterations to the services icons to improve consistency throughout the site. Also created JSON for the pricing section to remove duplicate markup.

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -1,47 +1,81 @@
 <template>
-<section class="pricing">
-  <h2 v-scroll-reveal.reset:false="{ delay: 180 }">Pricing</h2>
-  <p class="subtitle" v-scroll-reveal.reset:false="{ delay: 180 }">Flexible pricing plans.</p>
-	<div class="flex-container" v-scroll-reveal.reset:false="{ delay: 180 }">
-  <div class="flex-item">
-    <ul class="package">
-      <li class="header green">Basic</li>
-      <li class="gray">£20/month</li>
-      <li>20GB Storage</li>
-      <li>100 Domains</li>
-      <li>1TB Bandwidth</li>
-      <li>1000 Email Boxes</li>
-      <li class="gray">
-        <button class="green">Add to Cart</button>
-      </li>
-    </ul>
-  </div>
-  <div class="flex-item">
-    <ul class="package">
-      <li class="header purple">Pro</li>  
-      <li class="gray">£30/month</li>
-      <li>100GB Storage</li>
-      <li>500 Domains</li>
-      <li>2TB Bandwidth</li>
-      <li>1000 Email Boxes</li>
-      <li class="gray">
-        <button class="purple">Add to Cart</button>
-      </li>
-    </ul>
-  </div>
-  <div class="flex-item">
-   <ul class="package">
-      <li class="header pink">Premium</li>
-      <li class="gray">£60/month</li>
-      <li>250GB Storage</li>
-      <li>500 Domains</li>
-      <li>5TB Bandwidth</li>
-      <li>1000 Email Boxes</li>
-      <li class="gray">
-        <button class="pink">Add to Cart</button>
-      </li>
-    </ul>
-  </div>
-</div>
+  <section class="pricing">
+    <h2 v-scroll-reveal.reset:false="{ delay: 180 }">Pricing</h2>
+    <p class="subtitle" v-scroll-reveal.reset:false="{ delay: 180 }">Flexible pricing plans.</p>
+    <div class="flex-container" v-scroll-reveal.reset:false="{ delay: 180 }">
+      <div class="flex-item" v-for="(pricing, pricingIndex) in pricings" :key="'pricing' + pricingIndex">
+        <div class="package">
+          <h3 class="header">{{ pricing.title }}</h3>
+          <p class="gray">{{ pricing.price }}</p>
+            <ul>
+              <li 
+              v-for="(service, serviceIndex) in pricing.services"
+              :key="'pricing' + pricingIndex + 'service' + serviceIndex">
+              {{ service }}
+              </li>
+            </ul>
+          <div class="gray">
+              <button class="green">{{ pricing.button }}</button>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="flex-item">
+          <div class="package">
+            <h3 class="header purple">Premium</h3>
+            <p class="gray">£20/month</p>
+            <ul>
+              <li>100GB Storage</li>
+              <li>500 Domains</li>
+              <li>2TB Bandwidth</li>
+              <li>1000 Email Boxes</li>
+            </ul>
+            <div class="gray">
+              <button class="purple">Add to Cart</button>
+            </div>
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="package">
+            <h3 class="header pink">Pro</h3>
+            <p class="gray">£20/month</p>
+            <ul>
+              <li>250GB Storage</li>
+              <li>500 Domains</li>
+              <li>5TB Bandwidth</li>
+              <li>1000 Email Boxes</li>
+            </ul>
+            <div class="gray">
+              <button class="pink">Add to Cart</button>
+            </div>
+          </div>
+      </div> -->
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    pricings: [
+      {
+        title: "Basic",
+        price: "£20/month",
+        services: ["20GB Storage", "100 Domains", "1TB Bandwidth", "1000 Email Boxes"],
+        button: "Add to cart"
+      },
+      {
+        title: "Premium",
+        price: "£30/month",
+        services: ["100GB Storage", "500 Domains", "2TB Bandwidth", "2500 Email Boxes"],
+        button: "Add to cart"
+      },
+      {
+        title: "Pro",
+        price: "£50/month",
+        services: ["250GB Storage", "500 Domains", "5TB Bandwidth", "5000 Email Boxes"],
+        button: "Add to cart"
+      }
+    ]
+  })
+}
+</script>
